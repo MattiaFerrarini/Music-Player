6a07b53c9f667bd76a4410caf75fb790ffe02a96
Mattia Ferrarini <mattiaferrarini22@gmail.com>
2024-04-04 16:21:37 +0200

    Added code for sliders and time display

--- README.md
+++ README.md
@@ -2,9 +2,28 @@
 A music player website based on Bootstrap cards. Developed as an exercise for the Web Technologies course at University of Bologna.
 
 ## Credits
-Images:
+
+### Images
 - Photo by Dids .: https://www.pexels.com/photo/yellow-and-black-vinyl-disc-record-1616470/
 - Photo by Ashutosh Sonwani: https://www.pexels.com/photo/person-doing-tricks-on-cassette-tape-1762578/
 - Photo by Arturo  A: https://www.pexels.com/photo/pile-of-cd-discs-14466246/
 - Photo by Dominika Roseclay: https://www.pexels.com/photo/photography-of-vinyl-records-on-wooden-surace-1021876/
 - Photo by Wendy Wei: https://www.pexels.com/photo/purple-fireworks-effect-1190298/
+
+### Songs 
+- Cartoon, Jéja - On & On (feat. Daniel Levi) [NCS Release]
+    - Music provided by NoCopyrightSounds
+    - Free Download/Stream: http://ncs.io/onandon
+    - Watch: http://youtu.be/K4DyBUG242c
+- Janji - Heroes Tonight (feat. Johnning) [NCS Release]
+    - Music provided by NoCopyrightSounds
+    - Free Download/Stream: http://ncs.io/ht
+    - Watch: http://youtu.be/3nQNiWdeH2Q
+- DEAF KEV - Invincible [NCS Release]
+    - Music provided by NoCopyrightSounds
+    - Free Download/Stream: http://ncs.io/invincible
+    - Watch: http://youtu.be/J2X5mJ3HDYE
+- Different Heaven & EH!DE - My Heart [NCS Release]
+    - Music provided by NoCopyrightSounds
+    - Free Download/Stream: http://ncs.io/myheart
+    - Watch: http://youtu.be/jK2aIUmmdP4
--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let currentSongIndex;
 const songButtonPrefix = "songButton"
 const coverPrefix = "Assets/Covers/cover"
+let isPlaying;
+
+let musicPlayer, progressSlider, currentTimeDisplay, totalDurationDisplay;
 
 // all the following data should be in a database
-const titles = ["Song 0", "Song 1", "Song 2", "Song 3"]
-const authors = ["Author 0.1, Author 0.2", "Author 1", "Author 2", "Author 3"]
+const titles = ["On & On", "Heroes Tonight", "Invincible", "My Heart"]
+const authors = ["Cartoon, Jéja, Daniel Levi", "Janji, Johnning", "DEAF KEV", "Different Heaven & EH!DE"]
 const bgColors = ["#FFF", "#e6c993", "#AFE0FF", "#C7A0C7"];
 const descriptions = [
     "Morbi et fringilla eros. Fusce laoreet, neque at lacinia convallis, risus nisi semper nibh, eu mollis velit purus eget lectus. Phasellus interdum pretium elementum. Donec rhoncus, dolor id bibendum ullamcorper, enim enim imperdiet urna, vel tincidunt mi odio vel nunc. Proin laoreet ultrices lectus, vitae gravida massa hendrerit accumsan.",
@@ -12,6 +15,49 @@ const descriptions = [
     "Morbi eu metus tristique, dictum nibh eget, posuere metus. Quisque eget ante ut sem pulvinar porttitor a in leo. Ut elementum, mauris ut pulvinar accumsan, neque massa fringilla sapien, quis tempus mauris turpis suscipit quam. Praesent consectetur condimentum justo a pretium. Etiam tellus elit, pharetra ac eleifend nec, lacinia nec elit.",
     "Vivamus laoreet ligula et augue varius faucibus. Maecenas vestibulum turpis a nibh lobortis, et tincidunt massa laoreet. Donec ultricies fringilla nibh, lacinia suscipit nibh egestas eu. Nulla hendrerit felis vel tristique pellentesque. Proin id ante nec elit vulputate laoreet. Sed urna metus, luctus quis faucibus varius, porttitor vitae dui."
 ]
+const songFiles = ["Assets/Songs/Cartoon, Jéja - On & On (feat. Daniel Levi) [NCS Release].mp3", 
+    "Assets/Songs/Janji - Heroes Tonight (feat. Johnning) [NCS Release].mp3", 
+    "Assets/Songs/DEAF KEV - Invincible [NCS Release].mp3", 
+    "Assets/Songs/Different Heaven & EH!DE - My Heart [NCS Release].mp3"]
+
+window.onload = function(){
+    musicPlayer = document.getElementById("musicPlayer");
+    progressSlider = document.getElementById("progressSlider");
+    currentTimeDisplay = document.getElementById("currentTime");
+    totalDurationDisplay = document.getElementById("totalDuration");
+
+    initialize();
+}
+
+function initialize(){
+    initializesongList();
+
+    currentSongIndex = 0;
+    changeSong(currentSongIndex);
+
+    isPlaying = false;
+}
+
+function initializesongList(){
+    let songList = document.getElementById("songList");
+
+    for (var i = 0; i < titles.length; i++){
+        let newSongItem = document.createElement("li");
+        newSongItem.classList.add("list-group-item");
+
+        // Create a new button element
+        let newButton = document.createElement("button");
+        newButton.setAttribute("id", "songButton" + i);
+        newButton.setAttribute("onclick", "changeSection(" + i + ")");
+        newButton.textContent = titles[i];
+
+        // Append the button to the li element
+        newSongItem.appendChild(newButton);
+
+        // Append the li element to the ul
+        songList.appendChild(newSongItem);
+    }
+}
 
 function changeSong(songIndex){
     // change bolded item in side bar
@@ -31,10 +77,18 @@ function changeSong(songIndex){
     // change cover image
     document.getElementById("songCover").src = coverPrefix + songIndex + ".jpg";
 
+    // change song file in player
+    document.getElementById("musicPlayer").src = songFiles[songIndex];
+
     // set background color
     document.body.style.backgroundColor = bgColors[songIndex];
 
     currentSongIndex = songIndex;
+}
+
+function changeSection(sectionIndex){
+    changeSong(sectionIndex);
+    pause();
 }
 
 function nextSong(){
@@ -46,37 +100,66 @@ function previousSong(){
 }
 
 function nextPrevSong(offset){
+    // change song
     newSongIndex = (currentSongIndex + offset + titles.length) % titles.length;
     changeSong(newSongIndex);
+
+    // resume playing if already playing
+    if(isPlaying)
+        playSong();
 }
 
-function initialize(){
-    initializesongList();
-    currentSongIndex = 0;
-    changeSong(currentSongIndex);
+// Play/Pause toggle function
+function togglePlayPause() {
+    if(isPlaying)
+        pauseSong();
+    else
+        playSong();
 }
 
-function initializesongList(){
-    let songList = document.getElementById("songList");
-
-    for (var i = 0; i < titles.length; i++){
-        let newSongItem = document.createElement("li");
-        newSongItem.classList.add("list-group-item");
-
-        // Create a new button element
-        let newButton = document.createElement("button");
-        newButton.setAttribute("id", "songButton" + i);
-        newButton.setAttribute("onclick", "changeSong(" + i + ")");
-        newButton.textContent = titles[i];
-
-        // Append the button to the li element
-        newSongItem.appendChild(newButton);
-
-        // Append the li element to the ul
-        songList.appendChild(newSongItem);
-    }
+function pauseSong(){
+    playPauseIcon = document.getElementById('playPauseIcon');
+    musicPlayer.pause();
+    playPauseIcon.classList.remove('bi-pause-circle-fill');
+    playPauseIcon.classList.add('bi-play-circle-fill');
+    isPlaying = false;
 }
 
-window.onload = function(){
-    initialize();
+function playSong(){
+    playPauseIcon = document.getElementById('playPauseIcon');
+    musicPlayer.play();
+    playPauseIcon.classList.remove('bi-play-circle-fill');
+    playPauseIcon.classList.add('bi-pause-circle-fill');
+    isPlaying = true;
+}
+
+function onSongEnd(){
+    nextSong();
+}
+
+function onSongLoaded(){
+    totalDurationDisplay.textContent = formatTime(musicPlayer.duration);
+    progressSlider.value = 0;
+    currentTimeDisplay.textContent = formatTime(0);
+}
+
+function onTimeUpdate(){
+    const percentage = (musicPlayer.currentTime / musicPlayer.duration) * 100;
+    progressSlider.value = percentage;
+    currentTimeDisplay.textContent = formatTime(musicPlayer.currentTime);
+}
+
+function onSliderInput(){
+    const seekTime = (musicPlayer.duration / 100) * progressSlider.value;
+    musicPlayer.currentTime = seekTime;
+}
+
+function onVolumeInput(){
+    musicPlayer.volume = document.getElementById('volumeControl').value;
+}
+
+function formatTime(seconds) {
+    let minutes = Math.floor(seconds / 60);
+    seconds = Math.floor(seconds % 60);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
